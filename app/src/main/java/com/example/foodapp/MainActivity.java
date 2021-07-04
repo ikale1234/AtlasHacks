5066d07e800f1ec5cbf95b414b89bccbe71e1c98
vadog <vadog2672@gmail.com>
2021-07-03 17:57:52 -0700

    pushed andriod studio test app

---
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText item;
+    private Button addItem, removeItem;
+    private ListView list;
+    private ArrayList <String> arrayList;
+    private ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        item =findViewById(R.id.et_Item);
+        addItem = findViewById(R.id.btn_addItem);
+        list = findViewById(R.id.lv);
+        removeItem = findViewById(R.id.btn_removeItem);
+
+        arrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(adapter);
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.add(item.getText().toString());
+                adapter.notifyDataSetChanged();
+                item.getText().clear();
+            }
+        });
+        removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.remove(arrayList.size()-1);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        nextButton();
+
+    }
+    public void nextButton (){
+        Button findButton = findViewById(R.id.btn_findFood);
+        findButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Foods.class));
+            }
+        });
+    }
+}
